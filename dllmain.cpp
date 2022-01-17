058e2af3af6833cb3a4ffd66c5c43284b7ac71f3
Sr. Meia-noiteλ (LordZarkares) <73501736+LordZarkares@users.noreply.github.com>
2022-01-16 23:37:19 -0300

    Testing dll to inject.

---
+++ dllmain.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <windows.h>
+
+BOOL WINAPI DllMain(HANDLE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+        case DLL_PROCESS_ATTACH:
+            MessageBox(0, "Hello from the dllmain file!", "Hey!", 0);
+            break;
+        case DLL_PROCESS_DETACH:
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+            break;
+    }
+
+    return TRUE;
+}
